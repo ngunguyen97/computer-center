ed47cbffffd6075a20eb63feed300c83264bacf4
lucynguyen <ngu.nguyen@kyanon.digital>
2021-04-13 20:30:13 +0700

    Creating Category and CategoryClassroom Models.

---
+++ app/CategoryClassroom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryClassroom extends Model
+{
+  protected $table = 'category_classroom';
+  protected $guarded = [];
+}
