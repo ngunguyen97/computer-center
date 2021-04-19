3c1ec0c61a3a854529f222c85fe65614b5410d2b
lucynguyen <ngu.nguyen@kyanon.digital>
2021-04-19 22:13:37 +0700

    Adding convert number format to integer functionality.

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,10 @@ function presentPrice($price)
 {
   return  number_format($price) . 'đ';
 }
+
+function convertNumberFormatToInteger($number_format) {
+  return (int) filter_var($number_format, FILTER_SANITIZE_NUMBER_INT);
+}
 function setActiveCategory($category, $output = 'active')
 {
   return request()->category == $category ? $output : '';
