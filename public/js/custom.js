754f0ba3fe3c8fff182a7547710045659df648fe
lucynguyen <ngu.nguyen@kyanon.digital>
2021-05-23 21:59:19 +0700

    compile scss files

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,5 @@
+// var jQueryScript = document.createElement('script');
+// jQueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js');
+// document.head.appendChild(jQueryScript);
+
 
