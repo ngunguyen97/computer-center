e594884870533653d50186ecb28b44ba6ebb02ed
lucynguyen <ngu.nguyen@kyanon.digital>
2021-05-26 14:21:47 +0700

    Customizing re-examination flow.

--- app/ReExamination.php
+++ app/ReExamination.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ReExamination extends Model
 {
   protected $guarded = [];
+
+  public function classroom() {
+    return $this->belongsTo(Classroom::class);
+  }
+
+  public function order() {
+    return $this->belongsTo(Order::class);
+  }
+
+  public  function student() {
+    return $this->belongsTo(Student::class);
+  }
 }
