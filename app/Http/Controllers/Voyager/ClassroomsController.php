3ca543bf1601f6e87526f64fbeaa4407ccd190cf
lucynguyen <ngu.nguyen@kyanon.digital>
2021-05-26 14:23:47 +0700

    Updating: Classroom removes packages not use.

--- app/Http/Controllers/Voyager/ClassroomsController.php
+++ app/Http/Controllers/Voyager/ClassroomsController.php
@@ -2,17 +2,12 @@
 namespace App\Http\Controllers\Voyager;
 
 
-use Exception;
+
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use TCG\Voyager\Database\Schema\SchemaManager;
-use TCG\Voyager\Events\BreadDataAdded;
-use TCG\Voyager\Events\BreadDataDeleted;
-use TCG\Voyager\Events\BreadDataRestored;
-use TCG\Voyager\Events\BreadDataUpdated;
-use TCG\Voyager\Events\BreadImagesDeleted;
 use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController;
